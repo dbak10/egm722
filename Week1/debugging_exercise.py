5b1f115e118cd3131142f43a53e6a0cda4cf9cef
dbak10 <dbak10d@warpmail.net>
2025-03-13 14:59:18 +0000

    fix guessing game script
    change to print too high/ too low statement. Correction of range of random number generation.

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -2,7 +2,7 @@
 import random as random
 
 # pick a random number for the user to guess
-rand = random.randint(1, 100)
+rand = random.randint(1, 20)
 
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
@@ -11,7 +11,7 @@ while guess != rand:  # if the guess is not equal to the random number, you have
     if guess > rand:  # if the guess is too high, tell the user.
         print('Too high. Guess again.')
     else:  # if the guess is too low, tell the user.
-        print('Too high. Guess again.')
+        print('Too low. Guess again.')
 
     print('Enter a new guess: ')
     guess = int(input())
